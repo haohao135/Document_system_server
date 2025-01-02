6fc44f34a4a8e1c099e8934fa078125bdbdac99b
Vinny Nguyen <nguyntrungkin091@gmail.com>
2025-01-02 23:52:30 +0700

    update notification

---
+++ src/main/java/com/document/demo/models/Notification.java
@@ -0,0 +1,34 @@
+package com.document.demo.models;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "notifications")
+public class Notification {
+    @Id
+    private String notificationId;
+
+    @NotBlank(message = "message is required")
+    private String message;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    @Builder.Default
+    private boolean isRead = false;
+
+    @DBRef
+    private User user;
+
+    @DBRef
+    private Documents document;
+} 
